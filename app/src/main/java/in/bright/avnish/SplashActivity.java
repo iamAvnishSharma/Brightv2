eae6e2f7155629bbbeeb00bb3c02ff065090ae7a
Avnish <avnish.sh7@gmail.com>
2018-03-07 01:03:35 +0530

    splash activity

---
+++ app/src/main/java/in/bright/avnish/SplashActivity.java
@@ -0,0 +1,21 @@
+package in.bright.avnish;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Avnish on 07-Mar-18.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+        finish();
+    }
+}
