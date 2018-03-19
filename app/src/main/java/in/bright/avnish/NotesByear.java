a7e058a9eb3de2d9c810493a44ec5403e88d9a3b
ABHISHEK NIMBHORKAR <abhirnimbhorkar@gmail.com>
2018-03-19 23:17:57 +0530

    new notes with video background

--- app/src/main/java/in/bright/avnish/NotesByear.java
+++ app/src/main/java/in/bright/avnish/NotesByear.java
@@ -1,12 +1,15 @@
 package in.bright.avnish;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 public class NotesByear extends AppCompatActivity {
-
+VideoView v1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +46,16 @@ public class NotesByear extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), NotesBranch.class));
             }
         });
-
+        v1=(VideoView)findViewById(R.id.videoViewbyear);
+        Uri u= Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.bg);
+        v1.setVideoURI(u);
+        v1.start();
+        v1.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
 
 
 
--- app/src/main/java/in/bright/avnish/NotesStream.java
+++ app/src/main/java/in/bright/avnish/NotesStream.java
@@ -1,12 +1,15 @@
 package in.bright.avnish;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.VideoView;
 
 public class NotesStream extends AppCompatActivity {
-
+VideoView v1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +31,15 @@ public class NotesStream extends AppCompatActivity {
             }
         });
 
-
+        v1=(VideoView)findViewById(R.id.videoView_notes_stream);
+        Uri u= Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.bg);
+        v1.setVideoURI(u);
+        v1.start();
+        v1.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mediaPlayer.setLooping(true);
+            }
+        });
     }
 }
