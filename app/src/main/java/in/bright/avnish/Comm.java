cbb11101f2374af6f1b1e996b8c59572bb4006ab
Avnish <avnish.sh7@gmail.com>
2018-03-21 00:38:35 +0530

    static pages add

--- app/src/main/java/in/bright/avnish/Comm.java
+++ app/src/main/java/in/bright/avnish/Comm.java
@@ -1,13 +1,44 @@
 package in.bright.avnish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Comm extends AppCompatActivity {
+
+    ImageView i1,i2,i3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comm);
+
+
+        i1 = findViewById(R.id.imageView13);
+        i2 = findViewById(R.id.imageView14);
+        i3 = findViewById(R.id.imageView15);
+
+        i1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CommDesc.class));
+            }
+        });
+        i2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CommDesc.class));
+            }
+        });
+        i3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CommDesc.class));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/in/bright/avnish/Events.java
+++ app/src/main/java/in/bright/avnish/Events.java
@@ -1,13 +1,45 @@
 package in.bright.avnish;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Events extends AppCompatActivity {
+
+
+    ImageView i1,i2,i3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
+
+        i1 = findViewById(R.id.imageView6);
+        i2 = findViewById(R.id.imageView7);
+        i3 = findViewById(R.id.imageView8);
+
+        i1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), EventDesc.class));
+            }
+        });
+        i2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), EventDesc.class));
+            }
+        });
+        i3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), EventDesc.class));
+            }
+        });
+
+
     }
 }
